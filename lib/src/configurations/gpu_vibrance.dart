2184e67628b4ecc51a6d65f28ab764407e10862c
egorterekhov <asya.wear@gmail.com>
2023-01-13 12:37:24 -0800

    Add GPU filters from GPUSwirl to GPUZoomBlur

---
+++ lib/src/configurations/gpu_vibrance.dart
@@ -0,0 +1,20 @@
+part of flutter_gpu_video_filters;
+
+class GPUVibranceConfiguration extends GPUFilterConfiguration {
+  final NumberParameter _vibrance;
+
+  GPUVibranceConfiguration()
+      : _vibrance = _FloatParameter(
+          'inputVibrance',
+          'Vibrance',
+          0,
+        ),
+        super('Vibrance');
+
+  set vibrance(double value) {
+    _vibrance.value = value;
+  }
+
+  @override
+  List<ConfigurationParameter> get parameters => [_vibrance];
+}
